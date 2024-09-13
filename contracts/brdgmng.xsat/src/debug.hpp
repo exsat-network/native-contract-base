7dea37ffe1d7d9e581f677b3071e47d22f401c57
RobinHolmes <robin@exsat.org>
2024-09-13 16:08:35 +0800

    v5

--- contracts/brdgmng.xsat/src/debug.hpp
+++ contracts/brdgmng.xsat/src/debug.hpp
@@ -30,8 +30,13 @@ void brdgmng::cleartable(const name table_name, const optional<name> scope, cons
         address_mapping_index _address_mapping(get_self(), get_self().value);
         clear_table(_address_mapping, rows_to_clear);
     } else if (table_name == "deposits"_n) {
-        deposit_index _deposit(get_self(), get_self().value);
+        check(scope.has_value(), "brdgmng::cleartable: [scope] is required for deposits table");
+        deposit_index _deposit(get_self(), scope->value);
         clear_table(_deposit, rows_to_clear);
+    } else if (table_name == "withdraws"_n) {
+        check(scope.has_value(), "brdgmng::cleartable: [scope] is required for withdraws table");
+        withdraw_index _withdraw(get_self(), scope->value);
+        clear_table(_withdraw, rows_to_clear);
     } else {
         check(false, "brdgmng::cleartable: [table_name] unknown table to clear");
     }
