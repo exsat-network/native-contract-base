cbc9a4138ef8f72fe285c8db325dae480688c073
RobinHolmes <robin@exsat.org>
2024-09-23 14:46:01 +0800

    bridge tests

--- tests/build.sh
+++ tests/build.sh
@@ -6,11 +6,12 @@ cdt-cpp ../../contracts/exsat.xsat/exsat.xsat.cpp -I ../../contracts/ -DDEBUG
 cdt-cpp ../../contracts/poolreg.xsat/poolreg.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DDEBUG
 cdt-cpp ../../contracts/rescmng.xsat/rescmng.xsat.cpp -I ../../contracts/ -DDEBUG
 cdt-cpp ../../contracts/rwddist.xsat/rwddist.xsat.cpp -I ../../contracts/ -DUNITTEST -DDEBUG
-cdt-cpp ../../contracts/staking.xsat/staking.xsat.cpp -I ../../contracts/ -DDEBUG 
+cdt-cpp ../../contracts/staking.xsat/staking.xsat.cpp -I ../../contracts/ -DDEBUG
 cdt-cpp ../../contracts/blkendt.xsat/blkendt.xsat.cpp -I ../../contracts/ -DDEBUG
 cdt-cpp ../../contracts/endrmng.xsat/endrmng.xsat.cpp -I ../../contracts/ -DDEBUG
 cdt-cpp ../../contracts/blksync.xsat/blksync.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DMAINNET -DDEBUG
 cdt-cpp ../../contracts/utxomng.xsat/utxomng.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DMAINNET -DDEBUG
+cdt-cpp ../../contracts/brdgmng.xsat/brdgmng.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DMAINNET -DDEBUG
 
 wasm2wat btc.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o btc.xsat.wasm -
 wasm2wat exsat.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o exsat.xsat.wasm -
@@ -22,3 +23,4 @@ wasm2wat blkendt.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' |
 wasm2wat endrmng.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o endrmng.xsat.wasm -
 wasm2wat blksync.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o blksync.xsat.wasm -
 wasm2wat utxomng.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o utxomng.xsat.wasm -
+wasm2wat brdgmng.xsat.wasm | sed -e 's|(memory |(memory (export \"memory\") |' | wat2wasm -o brdgmng.xsat.wasm -
