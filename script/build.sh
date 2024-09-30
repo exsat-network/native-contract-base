f5095ee7ba13b3504ff51657163133510f31c230
OrsonBrooks <orson@exsat.org>
2024-09-30 15:05:00 +0800

    build: updaet build.sh

--- script/build.sh
+++ script/build.sh
@@ -7,10 +7,11 @@ cdt-cpp ../../contracts/poolreg.xsat/poolreg.xsat.cpp -I ../../contracts/ -I ../
 cdt-cpp ../../contracts/rescmng.xsat/rescmng.xsat.cpp -I ../../contracts/ -I ../../external  
 cdt-cpp ../../contracts/rwddist.xsat/rwddist.xsat.cpp -I ../../contracts/ -I ../../external 
 cdt-cpp ../../contracts/staking.xsat/staking.xsat.cpp -I ../../contracts/ -I ../../external 
+cdt-cpp ../../contracts/xsatstk.xsat/xsatstk.xsat.cpp -I ../../contracts/ -I ../../external 
 cdt-cpp ../../contracts/blkendt.xsat/blkendt.xsat.cpp -I ../../contracts/ -I ../../external 
 cdt-cpp ../../contracts/endrmng.xsat/endrmng.xsat.cpp -I ../../contracts/ -I ../../external 
-cdt-cpp ../../contracts/blksync.xsat/blksync.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DMAINNET
-cdt-cpp ../../contracts/utxomng.xsat/utxomng.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include -DMAINNET
+cdt-cpp ../../contracts/blksync.xsat/blksync.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include
+cdt-cpp ../../contracts/utxomng.xsat/utxomng.xsat.cpp -I ../../contracts/ -I ../../external -I ../../external/intx/include
 
 cleos set contract btc.xsat . btc.xsat.wasm btc.xsat.abi
 cleos set contract exsat.xsat . exsat.xsat.wasm exsat.xsat.abi
@@ -18,6 +19,7 @@ cleos set contract poolreg.xsat . poolreg.xsat.wasm poolreg.xsat.abi
 cleos set contract rescmng.xsat . rescmng.xsat.wasm rescmng.xsat.abi
 cleos set contract rwddist.xsat . rwddist.xsat.wasm rwddist.xsat.abi
 cleos set contract staking.xsat . staking.xsat.wasm staking.xsat.abi
+cleos set contract xsatstk.xsat . xsatstk.xsat.wasm xsatstk.xsat.abi
 cleos set contract blkendt.xsat . blkendt.xsat.wasm blkendt.xsat.abi
 cleos set contract endrmng.xsat . endrmng.xsat.wasm endrmng.xsat.abi
 cleos set contract blksync.xsat . blksync.xsat.wasm blksync.xsat.abi
@@ -25,10 +27,11 @@ cleos set contract utxomng.xsat . utxomng.xsat.wasm utxomng.xsat.abi
 
 cleos push action btc.xsat create '["btc.xsat", "21000000.00000000 BTC"]' -p btc.xsat
 cleos push action exsat.xsat create '["rwddist.xsat", "21000000.00000000 XSAT"]' -p exsat.xsat
-cleos push action blkendt.xsat config '[0, 4, 15]' -p blkendt.xsat
+cleos push action blkendt.xsat config '[0, 4, 15, 864000, 0, "21000.00000000 XSAT"]' -p blkendt.xsat
 cleos push action utxomng.xsat config '[600, 100, 1008, 100, 10, 10]' -p utxomng.xsat
 cleos push action utxomng.xsat init '[839999, '0000000000000000000172014ba58d66455762add0512355ad651207918494ab', '0000000000000000000000000000000000000000753b8c1eaae701e1f0146360']' -p utxomng.xsat
 cleos push action rescmng.xsat init '{"fee_account": "fees.xsat", "cost_per_slot": "0.00000001 BTC", "cost_per_upload": "0.00000001 BTC", "cost_per_verification": "0.00000001 BTC", "cost_per_endorsement": "0.00000001 BTC", "cost_per_parse": "0.00000001 BTC"}' -p rescmng.xsat
+cleos push action xsatstk.xsat setstatus '[false]' -p xsatstk.xsat
 
 cleos set account permission btc.xsat active '{
     "threshold": 1,
@@ -71,6 +74,13 @@ cleos set account permission staking.xsat active '{
         { "permission": { "actor": "staking.xsat", "permission": "eosio.code" }, "weight": 1 },
     ]
 }' -p staking.xsat;
+
+cleos set account permission xsatstk.xsat active '{
+    "threshold": 1,
+    "accounts": [
+        { "permission": { "actor": "xsatstk.xsat", "permission": "eosio.code" }, "weight": 1 },
+    ]
+}' -p xsatstk.xsat;
 
 cleos set account permission blkendt.xsat active '{
     "threshold": 1,
